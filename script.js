9fff6009ef376a96a90329c459a9cc4feaf15d1c
Jemlotelia <117761620+JemLotelia@users.noreply.github.com>
2023-03-30 18:30:35 +0100

    basic html and stareted on JS

---
+++ script.js
@@ -0,0 +1,8 @@
+function gridCreation (gridNumber) {
+    let gridArea = gridNumber * gridNumber;
+    for (i = 0; i < gridArea, i++){
+        something select container div
+        something create div 
+        something append to div
+    }
+}
