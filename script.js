4c16701d597c7ae994857409806d31df919f218a
Jemlotelia <117761620+JemLotelia@users.noreply.github.com>
2023-03-31 12:36:38 +0100

    add sizeSelect functionality

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const container = document.querySelector(".container")
-const color = 'black';
-
-
+const container = document.querySelector(".container");
+const sizeSelect = document.querySelector(".select")
+const baseColor = 'black';
 
 function gridCreation (gridNumber) {
+    let squares = container.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
     container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
     container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
     let gridArea = gridNumber * gridNumber;
@@ -13,3 +14,14 @@ function gridCreation (gridNumber) {
         container.insertAdjacentElement('beforeend', square)
     } 
 }
+
+
+sizeSelect.addEventListener('click', function changeSize(){
+    let number = prompt('What size? (Min: 2 Max: 100)')
+    if (number < 2 || number > 100){
+        alert('Number is either too big or small, choose a valid number')
+        changeSize()
+    }
+    gridCreation(number)
+})
+
