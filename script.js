67148f1467a29c0dd555c60452156c6ff3ef88d3
Jemlotelia <117761620+JemLotelia@users.noreply.github.com>
2023-03-31 12:14:02 +0100

    add gridCreation functioniality

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const container = document.querySelector(".container")
+const color = 'black';
+
+
+
 function gridCreation (gridNumber) {
+    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
+    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
     let gridArea = gridNumber * gridNumber;
-    for (i = 0; i < gridArea, i++){
-        something select container div
-        something create div 
-        something append to div
-    }
+    for (i = 0; i < gridArea; i++){
+        let square = document.createElement('div');
+        square.style.backgroundColor = `${color}`;
+        container.insertAdjacentElement('beforeend', square)
+    } 
 }
